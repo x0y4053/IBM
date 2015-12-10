24999cf97683783737f92c5832f91eacd1e44015
x0y4053 <vivianyu50@yahoo.com>
2015-12-10 12:35:26 -0600

    pulled data

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
-var API_Key = 'a5e95177da353f58113fd60296e1d250';
+var API_KEY = 'a5e95177da353f58113fd60296e1d250';
 var USER_ID = '24662369@N07';
 
+var photos = new Array();
+
+
 $().ready(function(){
-  $.getJSON("https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=API_KEY&user_id=USER_ID&format=json&nojsoncallback=1",
+  
+  $.getJSON("https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=" + API_KEY 
+  	+"&user_id=" + USER_ID +"&format=json&nojsoncallback=1",
     function(data){
-    var photosetID = "";
-    var title = "";
-    $.each(data.photos.photo, function(i,photo){
-    photosetID = set.id;
-    title = set.title._content;
-    ids.push(photosetID);
-    titles.push(title);
+      var ID = "";
+      var title = "";
+      $.each(data.photos.photo, function(i,sampleImgObj){	     
+	      var urlDefault = 'https://farm' + sampleImgObj.farm + '.staticflickr.com/' + sampleImgObj.server + '/' + sampleImgObj.id + '_' + sampleImgObj.secret + '.jpg';
+var urlLarge = 'https://farm' + sampleImgObj.farm + '.staticflickr.com/' + sampleImgObj.server + '/' + sampleImgObj.id + '_' + sampleImgObj.secret + '_b.jpg';
+		  var img = '<div class="row"> \
+		  				<div class="col-sm-6"><img src="' +urlDefault+'"</img></div> \
+		  				<div class="col-sm-6"> \
+		  					<div>Title:</div> \
+		  				    <div>' + sampleImgObj.title +'</div> \
+		  				    <div>ID:</div> \
+		  				    <div>' + sampleImgObj.id +'</div> \
+		  				    <div>Owner:</div> \
+		  				    <div>' + sampleImgObj.owner +'</div> \
+		  				    <div>Server:</div> \
+		  				    <div>' + sampleImgObj.server +'</div> \
+		  				</div>';
+
+		 
+	      photos.push(img);
+	     
     });
+    $("#gallery").html(photos);
 
   });
 });
